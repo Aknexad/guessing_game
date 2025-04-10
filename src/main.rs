8025f71bab2e8fe49327457908c23cc362439056
aknxad <alikhajoueinejad@gmail.com>
2025-04-10 21:04:39 +0330

    color guide

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,29 @@ fn main() {
  ╚═══════════════════════╝
 "
     );
+
+    println!(
+        "{}",
+        Colorize::truecolor(
+            "
+ ╔═══════════════════════╗
+ ║  🎯  Color Guide 🎯   ║
+ ╚═══════════════════════╝
+",
+            150,
+            0,
+            237
+        )
+    );
+
+    println!("{}", Colorize::green("██████████ => Win"));
+    println!("{}", Colorize::bright_green("██████████ => less then 5"));
+    println!("{}", Colorize::yellow("██████████ => less then 10"));
+    println!("{}", Colorize::bright_red("██████████ => less then 20"));
+    println!("{}", Colorize::red("██████████ => more then 20"));
     let mut score = 0;
     let secret_number = rand::random_range(1..=101);
-    println!("secret_number is {}", secret_number);
+    //println!("secret_number is {}", secret_number);
     loop {
         println!("Please input your guess.");
         let user_guess = utils::read_terminal();
